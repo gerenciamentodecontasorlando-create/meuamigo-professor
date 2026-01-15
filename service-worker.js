ad77364f20448a2eb4034b530a66eb9fff8cbc12
gerenciamentodecontasorlando-create <gerenciamentodecontasorlando@gmail.com>
2026-01-14 23:12:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,35 @@
-self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open("andy").then(c=>c.addAll([
-   "./",
-   "./index.html",
-   "./styles.css",
-   "./app.js"
-  ]))
- );
+const CACHE = "andy-pro-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+  self.skipWaiting();
 });
-self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : Promise.resolve())))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return resp;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
 });
